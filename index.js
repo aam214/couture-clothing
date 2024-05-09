79d927a725ba1ed9440c36a7f64172adffa6cb13
Ariana <152944814+aam214@users.noreply.github.com>
2024-05-08 21:02:52 -0500

    Renamed id for each item. Organized JS for easier reading. Added id to onClick for increment and decrement. Added functions for increment and decrement and passed id as an argument.

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 let shop =document.getElementById('store');
-let storeItemsData = 
-[{ id: "Clothing Item1", name: "Silk Dress",
+
+let storeItemsData = [
+  { id: "clothingItem1", 
+  name: "Silk Dress",
 price: 50,  
-img: "pictures/off-shoulder-dress.jpg",},
-{id: "Clothing Item2", name: "Sequin Dress",
+img: "pictures/off-shoulder-dress.jpg",} ,
+{id: "clothingItem2", 
+name: "Sequin Dress",
 price: 99, 
-img: "pictures/sequin-dress.jpg",},
-{id: "Clothing Item3", name: "Casual Dress",
+img: "pictures/sequin-dress.jpg",} ,
+{id: "clothingItem3", 
+name: "Casual Dress",
 price: 70,
-img: "pictures/leather-jacket.jpg",},
-{ id: "Clothing Item4", name: "Summer Dress",
+img: "pictures/leather-jacket.jpg",} ,
+{ id: "clothingItem4", 
+name: "Summer Dress",
 price: 58, 
-img: "pictures/white-dress.jpg",},
-{ id: "Clothing Item5", name: "Spring Dress",
+img: "pictures/white-dress.jpg",} ,
+{ id: "clothingItem5", 
+name: "Spring Dress",
 price: 40, 
-img: "pictures/casual-dress.jpg"},
-{ id: "Clothing Item6", name: "Wedding Dress",
+img: "pictures/casual-dress.jpg",} ,
+{ id: "clothingItem6", 
+name: "Wedding Dress",
 price: 570,
-img: "pictures/wedding.jpg",},
-{id: "Clothing Item7", name: "Formal Dress",
+img: "pictures/wedding.jpg",} ,
+{id: "clothingItem7", 
+name: "Formal Dress",
 price: 150, 
-img:"pictures/bridesmaid.jpg", },
-{id: "Clothing Item8", name: "Chiffon Dress",
+img:"pictures/bridesmaid.jpg",} ,
+{id: "clothingItem8", 
+name: "Chiffon Dress",
 price: 90, 
-img:"pictures/red-oneshoulder.jpg",}]
+img:"pictures/red-oneshoulder.jpg",}
+];
 
 let generateStore = () =>{
 
@@ -32,15 +42,15 @@ let {id, name, price, img} =x;
 
   return `
 <div id=product-id-${id} class="card">
-  <img src=${img} class="img-fluid rounded shadow" alt="Silk Dresses">
+  <img src=${img} class="img-fluid rounded shadow" alt="Dresses">
   <div class="card-body">
     <p class="card-text">${name} </p>
 <div class="cost">
 <strong>$ ${price} </strong>
 <span class="quantity">
-<i class="fa-solid fa-minus" onClick="decrement()"></i>
+<i class="fa-solid fa-minus" onClick="decrement(${id})"></i>
 <span id=${id} class="number">0</span>
-<i class="fa-solid fa-plus" onClick="increment()"></i>
+<i class="fa-solid fa-plus" onClick="increment(${id})"></i>
 </span>
 </div>
 <div class="click">Add to cart</div>
@@ -52,13 +62,15 @@ let {id, name, price, img} =x;
 
 generateStore(); 
   
-let increment = () => {
+let increment = (id) => {
+let selectedItem= id;
+console.log(selectedItem.id);
 
-console.log("increment");
 };
 
-let decrement = () => {
-  console.log("decrement");
-};
+let decrement = (id) => {
+  let selectedItem= id;
+  console.log(selectedItem.id);
+};  
 
 
