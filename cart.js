2bc02a3f0b17a9cedfe085f84db2fdb2218099d8
Ariana <152944814+aam214@users.noreply.github.com>
2024-05-11 22:23:50 -0500

    In dresses.html changed cartAmount id to amount. Added link to index page on cart page. Added HTML id to cart in navbar. Added 2 divs and 2 ids to shopping-cart. Cut and copied items array to item.js. Added HTML for empty cart into cart.js.

--- cart.js
+++ cart.js
@@ -1,9 +1,27 @@
-let cart = JSON.parse(localStorage.getItem("amount")) || [] ;
+let total = document.getElementById("total");
+let cartSaved =document.getElementById("cart-saved");
 
-let calculation =()=>{
-  let cartIcon = document.getElementById("cartAmount");
- 
-cartIcon.innerHTML =cart.map((x)=> x.item).reduce((x,y)=> x+y,0);
+
+let cart = JSON.parse(localStorage.getItem("amount")) || [ ] ;
+
+let calculation = () => {
+  let cartIcon = document.getElementById("amount");
+  cartIcon.innerHTML = cart.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
 
 calculation();
+
+
+let generateSavedItems = ()=>{
+  if(cart.length !==0){
+    console.log("Items are in cart!");
+  }else{
+cartSaved.innerHTML =``;
+total.innerHTML = `
+ <div class="cart-message">Cart is Empty!</div>
+ <a href="index.html" class="home-button" title="Back to Homepage">Back to Home</a>
+ `;
+  }
+
+};
+generateSavedItems();
--- index.js
+++ index.js
@@ -1,40 +1,5 @@
 let shop =document.getElementById('store');
 
-let storeItemsData = [
-  { id: "clothingItem1", 
-  name: "Silk Dress",
-price: 50,  
-img: "pictures/off-shoulder-dress.jpg",} ,
-{id: "clothingItem2", 
-name: "Sequin Dress",
-price: 99, 
-img: "pictures/sequin-dress.jpg",} ,
-{id: "clothingItem3", 
-name: "Casual Dress",
-price: 70,
-img: "pictures/leather-jacket.jpg",} ,
-{ id: "clothingItem4", 
-name: "Summer Dress",
-price: 58, 
-img: "pictures/white-dress.jpg",} ,
-{ id: "clothingItem5", 
-name: "Spring Dress",
-price: 40, 
-img: "pictures/casual-dress.jpg",} ,
-{ id: "clothingItem6", 
-name: "Wedding Dress",
-price: 570,
-img: "pictures/wedding.jpg",} ,
-{id: "clothingItem7", 
-name: "Formal Dress",
-price: 150, 
-img:"pictures/bridesmaid.jpg",} ,
-{id: "clothingItem8", 
-name: "Chiffon Dress",
-price: 90, 
-img:"pictures/red-oneshoulder.jpg",}
-
-];
 
 let cart = JSON.parse(localStorage.getItem("amount")) || [] ;
 
@@ -83,31 +48,26 @@ localStorage.setItem("amount", JSON.stringify(cart));
 
 let decrement = (id) => {
   let selectedItem= id;
-let search = cart.find((x) =>x.id ===selectedItem.id);
-if (search ===undefined) return;
-else if (search.item ===0) return;
+let search = cart.find((x) =>x.id === selectedItem.id);
+if (search === undefined) return;
+else if (search.item === 0) return;
 else{
   search.item -=1;
 }
 update(selectedItem.id);
 cart = cart.filter ((x) => x.item !==0);
-
 localStorage.setItem("amount",JSON.stringify(cart));
 };
 
 let update = (id)=>{
   let search =cart.find((x) =>x.id=== id);
-
   document.getElementById(id).innerHTML =search.item;
 calculation();
-
 };
 
-let calculation =()=>{
-  let cartIcon = document.getElementById("cartAmount");
-  //console.log(cart.map((x) => x.item).reduce((x,y)=> x+y,0));
-cartIcon.innerHTML =cart.map((x)=> x.item).reduce((x,y)=> x+y,0);
+let calculation = () => {
+  let cartIcon = document.getElementById("amount");
+  cartIcon.innerHTML = cart.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
 
 calculation();
-
