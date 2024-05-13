1c3dd50677f7cd919ab8bc1e00c520dc5ef3081d
Ariana <152944814+aam214@users.noreply.github.com>
2024-05-13 13:07:20 -0500

    Removed duplicate price in the shopping cart. Added + and - into cart with the quantity.

--- cart.js
+++ cart.js
@@ -21,6 +21,7 @@ let generateCartItems = ()=> {
 let search = mergedArray.find((x) => x.id === id) || [];
 let { img, price, name } = search;
 return `
+<div class="summary">
 <div class="card">
   <div class="card-header">
   ${search.name}
@@ -29,9 +30,15 @@ return `
   <div class="x"><i class="fa-solid fa-x"></i></div>
   <img src =${search.img} width= "200" class= "ms-4 img-fluid rounded shadow ms-3"/>
     <h5 class="card-title"></h5>
-    <p class="card-text">$${price}</p>
+    <p class="card-text">$${search.price}</p>
+    <span class="quantity">
+    <i class="fa-solid fa-minus" onClick="decrement(${id})"></i>
+    <span id=${id} class="number">${item}</span>
+    <i class="fa-solid fa-plus" onClick="increment(${id})"></i>
+    </span>
     </div>
   </div>
+</div>
 </div>
 
 `;
