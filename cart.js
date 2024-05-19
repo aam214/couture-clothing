e9aff1592ff067d19471391f6a5013be52437681
Ariana <152944814+aam214@users.noreply.github.com>
2024-05-19 12:15:08 -0500

    restructured HTML in cart.js and edited styling.

--- cart.js
+++ cart.js
@@ -22,22 +22,20 @@ let search = mergedArray.find((y) => y.id === id) || [];
 let { img, price, name } = search;
 return `
 <div class="summary">
-<div class="cards">
-  <h4 class="card-header ms-2">${name}</h4>
-  <div class="shopping-cart-details">
-<i onClick= "removeItem(${id})" class="fa-solid fa-x"></i>
-  <img src =${img} width= "150" class= "rounded shadow ms-4"/>
-    <p class="cost-of-item">$${price}</p>
-    <div>
-    <i class="fa-solid fa-minus" onClick="decrement(${id})"></i>
-    <span id=${id} class="number">${item}</span>
-    <i class="fa-solid fa-plus" onClick="increment(${id})"></i>
-    <span class="total-cost">$ ${item * price}</span>
-    </div>
-  </div>
-  </div>
-</div>
-
+  <div class="shopping-cart-cards">
+  <h4 class="card-header m-3">${name}</h4>
+  <i onClick= "removeItem(${id})" class="fa-solid fa-x"></i>
+    <img src =${img} width= "140" class= "rounded shadow ms-4"/>
+      <div class="shopping-cart-details">
+      <div class="cost-of-item">$${price}</div>
+      <div class="quantity-in-cart">
+      <i class="fa-solid fa-minus" onClick="decrement(${id})"></i>
+      <span id=${id} class="number">${item}</span>
+      <i class="fa-solid fa-plus" onClick="increment(${id})"></i>
+      </div>
+      <div class="total-cost">$ ${item * price}</div>
+      </div>
+      </div>
 `;
  })
  .join(""));
